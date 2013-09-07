1422e6949cf058bdb881e3d1d395e2019ea8214a
Thomas Rieder <tho.rieder@aon.at>
2013-09-08 01:07:15 +0200

    almost complete

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 
 $(function() {
 
+    // create menu
     $("a[href^='#']").on('click', function(e) {
      e.preventDefault();
 
@@ -8,13 +9,13 @@ $(function() {
      var bootstrap_fix = -60;
 
      if(this.hash !== "#top") {
-        target = $(this.hash).offset().top + bootstrap_fix;
+      target = $(this.hash).offset().top + bootstrap_fix;
     }
 
     $('html, body').animate({ scrollTop: target }, 300);
 
        // TODO edit: Opera and IE requires the "html" elm. animated
-   });
+     });
 
     $(document).scroll(function() {
 
@@ -26,12 +27,12 @@ $(function() {
         var headingPosition = $(this).offset().top;
 
         if(headingPosition > currentPosition) {
-            if(highlightIndex === -1) {
-                highlightIndex = index;
-            }
+          if(highlightIndex === -1) {
+            highlightIndex = index;
+          }
         }
 
-    });
+      });
 
       // id of the nearest header
       var targetId = $($("h2")[highlightIndex]).attr('id');
@@ -39,16 +40,28 @@ $(function() {
       // remove previous glow
       $("#navbar li a").each(function(index) {
         $(this).parent().removeClass("active");
-    });
+      });
 
       $("#navbar span").each(function(index) {
         $(this).removeClass("glyphicon-glow");
-    });
+      });
 
 
       // add fancy glow
       $('#navbar li a[href$="' + targetId + '"]').parent().addClass("active");
       $("span", $('#navbar li a[href$="' + targetId + '"]').parent()).addClass("glyphicon-glow");
 
+    });
+
+  // create hover effect for contact area
+  $("[class='contact-logo-hover']").hide();
+
+  $("#contact-area>ul>li>a").hover(function(e) {
+    $("[class='contact-logo-hover']", e.currentTarget).show();
+    $("[class='contact-logo']", e.currentTarget).hide();
+  },
+  function(e) {
+    $("[class='contact-logo-hover']", e.currentTarget).hide();
+    $("[class='contact-logo']", e.currentTarget).show();
   });
 });
